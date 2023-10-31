e6d3f48d059daf303d2f43b154bcb3745d6010ec
FERNANDO JOSÉ PEREIRA DE SOUSA <FERNANDO0156@DIGITALCOLLEGE.LOCAL>
2023-10-30 21:52:49 -0300

    Assincrono e Sincrono

--- index.js
+++ index.js
@@ -20,14 +20,14 @@
 
 //Programacao Síncrona - *** //
 
-const nome = "Miriam";
-const saudacao = `Olá, meu nome é ${nome}!`;
-console.log(nome);
-const result = fetch("https://fakestoreapi.com/products")
-  .then((res) => res.json())
-  .then((json) => json);
-console.log(result);
-console.log(saudacao);
+// const nome = "Miriam";
+// const saudacao = `Olá, meu nome é ${nome}!`;
+// console.log(nome);
+// const result = fetch("https://fakestoreapi.com/products")
+//   .then((res) => res.json())
+//   .then((json) => json);
+// console.log(result);
+// console.log(saudacao);
 
 /**
  * Criar uma função que ao clicar no btnList
@@ -36,11 +36,11 @@ console.log(saudacao);
  * Funções exemplo: renderDataTable e createTableBodyRow
  */
 
-
 // Assíncrona - 
 
 const list = document.getElementById("list");
 const btnList = document.getElementById("btn-list");
+
 
 async function getProducts() {
     const response = await fetch("https://fakestoreapi.com/products");
@@ -50,5 +50,20 @@ async function getProducts() {
 
 getProducts();
 
-//
+function createListItem(nome) {
+  const html = (list.innerHTML += `
+  <li>${nome}<li>
+  `);
+  return html;
+}
 
+async function renderList() {
+  const products = await getProducts();
+  list.innerHTML = "";
+  products.map((product) => {
+    const data = createListItem(product.title);
+    return data;
+  });
+}
+
+btnList.addEventListener ("click", renderList);
